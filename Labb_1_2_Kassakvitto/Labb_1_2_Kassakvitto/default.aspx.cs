c13ada9d976928c1d13dfa71ac02c06d523053ee
hg222dt <hg222dt@student.lnu.se>
2014-02-03 22:06:54 +0100

    löste autofocus och select med inline js, med slect() och focus(), istället.

---
+++ Labb_1_2_Kassakvitto/Labb_1_2_Kassakvitto/default.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Labb_1_2_Kassakvitto
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void CalculateButton_Click(object sender, EventArgs e)
+        {
+            //Validera
+            //göra med get istället för post
+            //pusha ut kvitto
+            // mm mm
+            
+
+            if (IsValid)
+            {
+                
+
+
+
+            }
+
+
+
+
+        }
+    }
+}
